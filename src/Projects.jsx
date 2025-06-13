ffb4a12dc44ca3bda8490cc75f167026850bc1b3
Manuel Codes <manuelfranky0@gmail.com>
2025-06-13 04:10:08 +0100

    project.jsx

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -21,9 +21,7 @@ const Projects = () => {
         </div>
 
         <div className="bg-gray-300 border border-gray-700 rounded-xl p-6">
-          <h1 className="text-2xl font-semibold mb-2">
-            Security Engineering
-          </h1>
+          <h1 className="text-2xl font-semibold mb-2">Security Engineering</h1>
           <h2 className="text-lg font-semibold mb-4">Wazuh Inc.</h2>
         </div>
 
@@ -44,7 +42,8 @@ const Projects = () => {
         <div className="bg-gray-300 border border-gray-700 rounded-xl p-6">
           <h1 className="text-2xl font-semibold mb-2">Intern</h1>
           <h2 className="text-lg font-semibold mb-4">
-            Network engineer Broadband services (Intern)
+            Network engineer Broadband services (Intern) Nigerian Communications
+            Satellite Limited (NIGCOMSAT)
           </h2>
         </div>
       </main>
